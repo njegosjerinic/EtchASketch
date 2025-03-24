1a5cc507b319f3b6d3119c4fda9fcf9359840450
Njegos Jerinic <njegos.jerinic@gmail.com>
2025-03-24 13:18:17 +0100

    Done

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 document.addEventListener('DOMContentLoaded',function(){
     const mainDiv = document.getElementById('container');
     const sizeInput = document.getElementById('sizeInput');
-    let i;
+    let i = 16;
+    
     
     sizeInput.addEventListener('click', function(){
-        prompt
+        i = Number(prompt("Enter the number of spaces"));
+        mainDiv.innerHTML = "";
+        sketchsMaker(i);
+        
     })
     
-
-    for(let j = 0;j <= i;j++){
-        let miniDivs = document.createElement('div');
+    let sketchsMaker = function(i){
+        for(let j = 0;j <= i;j++){
+            let miniDivs = document.createElement('div');
         
-        miniDivs.className = "bigdivs";
-        mainDiv.appendChild(miniDivs)
+            miniDivs.className = "bigdivs";
+            mainDiv.appendChild(miniDivs)
 
-        for(let k = 0; k <= i;k++){
-            let ultraminiDivs = document.createElement('div');
+            for(let k = 0; k <= i;k++){
+                let ultraminiDivs = document.createElement('div');
 
-            ultraminiDivs.style.height = "50px";
-            ultraminiDivs.style.width = "50px";
-            ultraminiDivs.className = "littleDivs";
-            miniDivs.appendChild(ultraminiDivs);
+                ultraminiDivs.style.height = "100%";
+                ultraminiDivs.style.width = "100%";
+                ultraminiDivs.className = "littleDivs";
+                miniDivs.appendChild(ultraminiDivs);
 
-            ultraminiDivs.addEventListener("mouseenter",(event) =>{
-                event.target.style.background = "black";
-            });
+                ultraminiDivs.addEventListener("mouseenter",(event) =>{
+                    event.target.style.background = "black";
+                });
+            }
         }
     }
 
     
-    
+    sketchsMaker(i);
 })
